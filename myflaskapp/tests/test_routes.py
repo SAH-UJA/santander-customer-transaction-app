3d691cf12d4a810530ed3ceeddc03f8fc84a2cf0
SAH-UJA <sahil27ahuja1999@gmail.com>
2024-02-10 01:01:19 +0530

    restrucured app directories

---
+++ myflaskapp/tests/test_routes.py
@@ -0,0 +1,7 @@
+# tests/test_routes.py
+
+# from myflaskapp.app.routes import app
+
+# def test_app():
+#     test_client = app.test_client()
+#     assert test_client.get("/").response == {"message": "OK"}
