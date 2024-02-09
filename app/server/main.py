c64dfbddea4e1d7832bb8b98d19203082aa250ee
SAH-UJA <sahil27ahuja1999@gmail.com>
2024-02-09 20:22:20 +0530

    cosmetic changes for pylint tests

--- app/server/main.py
+++ app/server/main.py
@@ -1,10 +1,14 @@
+"""Main flask app server module"""
+
 from flask import Flask
 
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
 def home():
+    """Home route"""
     return {"message": "OK"}
+
 
 if __name__ == "__main__":
     flask_app.run(threaded=True, debug=True)
--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,7 +1,10 @@
+"""This module covers basic API I/O tests"""
+
 from app.server.main import flask_app
 
 
 def test_index_route():
+    """Test to check basic output of home route"""
     response = flask_app.test_client().get('/')
 
     assert response.status_code == 200
