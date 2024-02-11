c3188f8ce2f9157d7bfa2bea61c1ffc2e9bd23f6
SAH-UJA <sahil27ahuja1999@gmail.com>
2024-02-11 18:22:59 +0530

    test cases fixed

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,8 +4,8 @@ Test cases for the Santander Customer Transactions Analytics FastAPI application
 These test cases use the FastAPI TestClient to test the API endpoints for file upload functionality.
 """
 
+from utils.config_reader import server_config, classification_config
 from fastapi.testclient import TestClient
-from server.core.config import settings
 from server.app import app
 from pathlib import Path
 import os
@@ -27,7 +27,9 @@ def test_upload_file():
     files = {"data_file": ("batch_of_two.csv", open(test_data_path, "rb"))}
 
     # Make the API call
-    route = f"{settings.API_V1_STR}/classification/inference/uploadfile"
+    route = (
+        f"{server_config['api_prefix']}{classification_config['api_prefix']}/uploadfile"
+    )
     response = client.post(route, files=files)
 
     # Assertions
@@ -46,7 +48,9 @@ def test_upload_file_no_file():
     - Ensure the response status code is 422 (Unprocessable Entity).
     """
     # Make the API call without a file
-    upload_route = f"{settings.API_V1_STR}/classification/inference/uploadfile"
+    upload_route = (
+        f"{server_config['api_prefix']}{classification_config['api_prefix']}/uploadfile"
+    )
     response = client.post(upload_route)
 
     # Assertions
