cfa894c36e761f0c5b0c92a67e55f18b813061b7
SAH-UJA <sahil27ahuja1999@gmail.com>
2024-02-11 18:16:56 +0530

    cosmetic changes

--- server/__main__.py
+++ server/__main__.py
@@ -1,11 +1,26 @@
-from server.core.config import settings
-import uvicorn
+"""
+Production-ready script to run the Uvicorn server for the Santander Customer Transaction App.
 
+This script initializes the Uvicorn server to run the FastAPI application for the Santander Customer Transaction App in a production environment.
+"""
+
+from utils.config_reader import server_config, classification_config
+import uvicorn
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "server.app:app",
-        host=settings.SERVER_HOST,
-        port=settings.SERVER_PORT,
-        reload=True
-    )
+    try:
+        # Run the application with Uvicorn
+        uvicorn.run(
+            "server.app:app",
+            host=server_config["host"],
+            port=server_config["port"],
+            reload=False,  # Turn off auto-reload for production
+            workers=4,  # Adjust the number of workers based on your system's capabilities
+        )
+    except Exception as e:
+        logger.error(f"Failed to start the application: {e}")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,53 @@
+"""
+Test cases for the Santander Customer Transactions Analytics FastAPI application.
+
+These test cases use the FastAPI TestClient to test the API endpoints for file upload functionality.
+"""
+
 from fastapi.testclient import TestClient
 from server.core.config import settings
 from server.app import app
 from pathlib import Path
 import os
-import pytest
 
+# Initialize the TestClient
 client = TestClient(app)
 
+
 def test_upload_file():
-    test_data_path = os.path.join(Path(__file__).parent, "test_data.csv")
-    files = {"data_file": ("test_data.csv", open(test_data_path, "rb"))}
+    """
+    Test the file upload endpoint with valid file data.
+
+    Assertions:
+    - Ensure the response status code is 200.
+    - Ensure the response content matches the expected content for successful file upload.
+    """
+    # Prepare test data
+    test_data_path = os.path.join(Path(__file__).parent, "data", "batch_of_two.csv")
+    files = {"data_file": ("batch_of_two.csv", open(test_data_path, "rb"))}
+
+    # Make the API call
     route = f"{settings.API_V1_STR}/classification/inference/uploadfile"
     response = client.post(route, files=files)
 
+    # Assertions
     assert response.status_code == 200
-    assert response.content == b'[{"ID_code":"test_0","target_pred":1},{"ID_code":"test_1","target_pred":1}]'
+    assert (
+        response.content
+        == b'[{"ID_code":"test_0","target_pred":1},{"ID_code":"test_1","target_pred":1}]'
+    )
 
 
 def test_upload_file_no_file():
+    """
+    Test the file upload endpoint without providing a file.
+
+    Assertions:
+    - Ensure the response status code is 422 (Unprocessable Entity).
+    """
+    # Make the API call without a file
     upload_route = f"{settings.API_V1_STR}/classification/inference/uploadfile"
     response = client.post(upload_route)
-    
+
+    # Assertions
     assert response.status_code == 422
